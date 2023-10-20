63df09be84b705203193a25c37f2eb6024a9d746
Caua Dickmann Labes <caua.labes@proway.treina>
2023-10-20 14:49:19 -0300

    Estudo sobre novos comandos da agenda

--- foreign.DDL.sql
+++ foreign.DDL.sql
@@ -15,7 +15,13 @@ email varchar(50) not null
 );
 
 insert into compromisso(ID_compromisso,descricao,local,data,hora)
-values(2, 'restaurante', 'rua abc', '2023-12-12','23:00:00');
+values(3, 'restaurante', 'rua abc', '2023-12-12','23:00:00');
+
+insert into compromissos(ID_contato,ID_compromisso)
+values(1,2);
+
+select * from compromisso where ID_compromisso = 2;
+select * from contato;
 
 create table compromissos(
 ID_contato int,
@@ -23,3 +29,13 @@ ID_compromisso int,
 constraint fk_contatos foreign key(ID_contato) references contato(ID_contato),
 constraint fk_compromisso foreign key(ID_compromisso) references compromisso(ID_compromisso)
 );
+
+/* compromissos = N para N
+ID_contato = primary key 
+ID_compromisso = primary key
+*/
+
+select descricao, nome, local
+from compromisso, contato, compromissos
+where compromissos.ID_contato = contato.ID_contato
+and compromissos.ID_compromisso = compromisso.ID_compromisso;
