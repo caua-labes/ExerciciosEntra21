e4266b01f80151bd399bbe43aea0f2e3a92693b8
caua-labes <caua_labes@estudante.sesisenai.org.br>
2024-02-20 17:11:02 -0300

    Aula explorando as funções de LINQ

---
+++ Back-End.C#/Aulas/Aula de LINQ/Estudante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_de_LINQ
+{
+    public class Estudante
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public int Idade { get; set;}
+        public List<int> notas { get; set; }
+        
+    }
+}
