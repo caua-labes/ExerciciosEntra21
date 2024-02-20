7dff0ba8ebd36b63c83daecbbd29d1b1a7a71de8
caua-labes <caua_labes@estudante.sesisenai.org.br>
2024-02-20 17:09:20 -0300

    Primeira Aula de Api

---
+++ Back-End.C#/Aulas/Aula de Api/Aula de Api/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula_de_Api.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ Back-End.C#/Aulas/Aula de Api/Aula de Api/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula_de_Api.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public bool Completo { get; set; }
+    }
+}
