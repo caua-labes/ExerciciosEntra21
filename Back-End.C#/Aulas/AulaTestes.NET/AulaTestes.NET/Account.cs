ce395b25a1d549356ec335bb34fbe19c807aada9
caua-labes <caua_labes@estudante.sesisenai.org.br>
2024-02-20 17:18:22 -0300

    Primeira aula de Teste de usuario

---
+++ Back-End.C#/Aulas/AulaTestes.NET/AulaTestes.NET/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaTestes.NET
+{
+    public class Account
+    {
+        public double m_balance {  get; set; }
+        public void Withdraw(double amount)
+        {
+            if (m_balance >= amount)
+            {
+                m_balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException(nameof(amount), "Saldo insuficiente");
+            }
+        }
+    }
+}
---
+++ Back-End.C#/Aulas/AulaTestes.NET/AulaTestes.NETTests/AccountTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AulaTestes.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaTestes.NET.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+
+        [TestMethod()]
+        public void StoreTest()
+        {
+            Account account = new Account();
+            account.m_balance = 10.0;
+            account.Withdraw(9.0);
+            double valor_esperado = 1.0;
+            Assert.AreEqual(valor_esperado, account.m_balance);
+        }
+
+        [TestMethod()]
+        public void WithdrawTest()
+        {
+            Account account = new Account();
+            account.m_balance = 10.0;
+            account.Withdraw(9.0);
+            double valor_esperado = 1.0;
+            Assert.AreEqual(valor_esperado, account.m_balance);
+        }
+
+        [TestMethod()]
+
+        public void WithdrawTestThrowException()
+        {
+            Console.ReadLine();
+            Account account = new();
+            account.m_balance = 10.0;
+            double valor_esperado = 1.0;
+            account.Withdraw(9.0);
+
+            ArgumentException exception = Assert.ThrowsException<ArgumentException>(() => account.Withdraw(2.0));
+            Assert.AreEqual("ola mundo", exception.Message);
+        }
+    }
+}
