28f637b2344890350ba4f7d7467c601fce56dae0
caua-labes <caua_labes@estudante.sesisenai.org.br>
2024-02-20 17:20:38 -0300

    Primeira atividade com programação orientada objeto POO

---
+++ Back-End.C#/Aulas/AtividadesPOO/Atividade3comSortedEList/Atividade3comSortedEList/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3comSortedEList
+{
+    class Produto
+    {
+        public string produto;
+        public string descricao;
+        public double preco;
+
+        public string ToString()
+        {
+            return $"{produto}\n{descricao}\n{preco}\n";
+        }
+    }
+}
---
+++ Back-End.C#/Aulas/AtividadesPOO/Atividade3comSortedEList/Atividade3comSortedEList/Program.cs
@@ -0,0 +1,88 @@
+﻿using Atividade3comSortedEList;
+
+namespace Atividade3comSortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, List<Produto>> lista = new SortedList<string, List<Produto>>();
+            List<Produto> lisProd = new List<Produto>();
+            while (true)
+            {
+                Produto produto = new Produto();
+                Console.Clear();
+                Console.WriteLine("1 - Inserir novo produto\n2 - Criar categoria\n3 - Ver lista\n");
+                int val = int.Parse(Console.ReadLine());
+
+                switch (val)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("Produto: ");
+                        produto.produto = Console.ReadLine();
+                        Console.Write("Descrição: ");
+                        produto.descricao = Console.ReadLine();
+                        Console.Write("Valor: ");
+                        produto.preco = double.Parse(Console.ReadLine());
+                        lisProd.Add(produto);
+                        int poslista = lisProd.Count;
+
+                        Console.WriteLine("\nCategoria do produto");
+                        string verAddProdCat = Console.ReadLine();
+                        if (lista.ContainsKey(verAddProdCat))
+                        {
+                            lista[verAddProdCat].Add(lisProd[poslista - 1]);
+                            Console.WriteLine("Produto adicionado");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Categoria inválida");
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Qual categoria deseja criar? ");
+                        string novaCategoria = Console.ReadLine();
+
+                        if (lista.ContainsKey(novaCategoria))
+                        {
+                            Console.WriteLine("Categoria inválida");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Categoria criada");
+                            lista.Add(novaCategoria, new List<Produto>());
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Qual categoria deseja ver");
+                        string CatVer = Console.ReadLine();
+                        if (lista.ContainsKey(CatVer))
+                        {
+                            Console.WriteLine($"{CatVer}\n");
+                            foreach (var list in lista[CatVer])
+                            {
+                                Console.WriteLine(list.ToString());
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Categoria inválida");
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
