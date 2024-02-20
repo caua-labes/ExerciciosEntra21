690c8b2bb48729715c114698e839b474bd69bf9b
caua-labes <caua_labes@estudante.sesisenai.org.br>
2024-02-20 17:19:18 -0300

    Primeira aula sobre solid em c#

---
+++ Back-End.C#/Aulas/Solid/SOLID/SOLID/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SOLID{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Crud func = new Crud();
+            while (true) { 
+
+            Produto produto = new Produto();
+            Console.WriteLine("1 - Inserir\n2 - Apagar\n3 - Alterar\n4 - Listar\n'");
+            int val = int.Parse(Console.ReadLine());
+                switch (val)
+                {
+                    case 1: 
+                        func.inserir();
+                        break;
+                    case 2:
+                        func.apagar();
+                        break;
+                    case 3:
+                        func.alterar();
+                        break;
+                    case 4:
+                        func.listar();
+                        break;
+                }
+            }
+        }
+    }
+}
